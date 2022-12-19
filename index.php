99c393cea57433709c09a52101b32daed0a450d5
Rocío Andrea Mamani Cruz <65911403+rocio2504@users.noreply.github.com>
2022-12-19 15:28:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,7 @@ echo " Formulario"
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <title>Document</title>
-    // link: https://getbootstrap.com/docs/5.0/getting-started/introduction/
-
+     <!-- link: https://getbootstrap.com/docs/5.0/getting-started/introduction/   -->
     <script src="main.js"></script>
 </head>
 <body>
